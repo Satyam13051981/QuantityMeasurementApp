bf88d8a4faa4d98a397e9be9419f8f7f17a91a74
Satyam13051981 <satyam2604can@gmail.com>
2026-01-30 11:40:41 +0530

    "Commiting for use case 2"

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -5,6 +5,11 @@ public class QuantityMeasurementApp {
         Feet feet = new Feet(2.34);
         Feet feet2 = new Feet(2.34);
 
-        System.out.println(feet.equals(feet2));
+        System.out.println("Is given feet equals: " + feet.equals(feet2));
+
+        Inches inches1 = new Inches(26.8);
+        Inches inches2 = new Inches(26.8);
+
+        System.out.println("Is given inches are equals: " + inches1.equals(inches2));
     }
 }
